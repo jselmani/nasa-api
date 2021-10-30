6b940780517bed2c1633448ab592d18b121564ad
Jiel Selmani <jiel.selmani@gmail.com>
2021-10-30 15:40:02 -0400

    Added Mongoose and connected to a MongoDB instance on Atlas

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
@@ -6,9 +7,21 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_ATLAS_PASSWORD = "aSQGAHVKuug9fhJf";
+const MONGO_URL = `mongodb+srv://nasa-api:${MONGO_ATLAS_PASSWORD}@nasa-api-cluster.tnbgr.mongodb.net/nasa?retryWrites=true&w=majority`;
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Error: ", err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
