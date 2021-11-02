5c9924bb6794596191436c50876dbba2ef1b07f4
Jiel Selmani <jiel.selmani@gmail.com>
2021-11-02 16:24:35 -0400

    Removed CORS package as plans for frontend development have changed

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const express = require("express");
-const cors = require("cors");
 const logger = require("morgan");
 
 const api = require("./routes/api");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
 app.use(logger("combined"));
 app.use(express.json());
 
